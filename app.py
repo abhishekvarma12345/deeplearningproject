b5c35cd998299a7c205139523c42a0b1f8301e6b
abhishekvarma12345 <abhishekvarmad@gmail.com>
2024-03-10 15:18:33 +0100

    updated app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 from pathlib import Path
 
 
-# started my pipeline
+# saving image and serving 
 def save_image(uploaded_file):
     if uploaded_file is not None:
         save_path = os.path.join("images", "input.jpeg")
